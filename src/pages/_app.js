78c9d881d379f3c7a842acf080e656349f1761a3
Renato Alencar <renatoalencar.73@gmail.com>
2024-08-21 09:33:19 -0300

    Add Casper Click

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import "@/styles/globals.css";
+import dynamic from "next/dynamic";
+
+const clickOptions = {
+    appName: 'Casper dApp',
+    appId: 'csprclick-template',
+    contentMode: 'iframe',
+    providers: ['casper-wallet', 'ledger', 'metamask-snap', 'casperdash'],
+};
+
+const ClickProvider = dynamic(
+  () => import('@make-software/csprclick-ui').then(imp => imp.ClickProvider),
+  { ssr: false },
+)
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ClickProvider options={clickOptions}>
+      <Component {...pageProps} />
+    </ClickProvider>
+  )
 }
